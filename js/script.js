7248fefeeb54723da6a411bf7ed27e208d19043c
Melissa Preece <melissanchez@gmail.com>
2019-11-07 20:26:29 -0800

    Fixed Search Function

--- js/script.js
+++ js/script.js
@@ -66,67 +66,71 @@ const appendPageLinks = (list) => {
 showPage(studentList, 1);
 appendPageLinks(studentList);
 
+
 // EXTRA CREDIT
 
-/*** Add functionality to the search component
-When the "Search" button is clicked, the list is filtered by student name for those that include 
-the search value. For example, if the name Phillip is typed into the box, list all items with a name 
-that includes Phillip. If the letter S is typed in, all items with an S in the name will show.
+// ELEMENTS OF THE SEARCH BAR
+const h2 = document.querySelector('h2');
+const studentSearchDiv = document.createElement('div');
+const searchBar = document.createElement('input');
+const searchButton = document.createElement('button');
+studentSearchDiv.classList.add("student-search");
+searchBar.placeholder = "Search for students...";
+searchButton.textContent = "Search";
+h2.insertAdjacentElement('afterend', studentSearchDiv);
+studentSearchDiv.appendChild(searchBar);
+studentSearchDiv.appendChild(searchButton);
 
-Pro Tip: To improve the functionality and add to the user experience, consider adding a keyup event 
-listener to the search input so that the list filters in real time as the user types. This would be 
-in addition to making the search button clickable since pasting text into the search bar might not 
-trigger the keyup event.
+// SEARCH BAR FUNCTION
+let searchResults = [];
+const nameSearch = (searchInput, names) => {
+   for (let i = 0; i < names.length; i += 1) {
+      names[i].classList.remove("student-search");
+      if (0 != searchInput.value.length && names[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+         names[i].classList.add("student-search");
+         searchResults += names[i];
+         return searchResults;
+      };
+   };
+};
 
-Project Warm Up: Configuring a search feature can seem complex at first, but it's really just a few 
-fundamental steps. For some helpful practice, check out the project Warm Up Simple Search.
+// SEARCH FUNCTION EVENT LISTENERS:
+   /* SEARCH BUTTON LISTENER */
+      searchButton.addEventListener('click', (event) => {
+         nameSearch(searchBar, studentList);
+         showPage(searchResults, 1);
+         appendPageLinks(searchResults);
+         //console.log('Submit button is functional');
+      });
 
-Paginate search results
+   /* SEARCH INPUT LISTENER */
+      searchBar.addEventListener('keyup', (event) => {
+         nameSearch(searchBar, studentList);
+         showPage(searchResults, 1);
+         appendPageLinks(searchResults);
+         //console.log('Keyup event on the search input is functional');
+      });
+
+
+/*** IN PROGRESS Paginate search results 
 Display pagination links based on how many search results are returned. For example: if 10 or fewer 
-results are returned, 0 or 1 pagination links are displayed. If 22 search results are returned, 3 pagination links are displayed.
+results are returned, 0 or 1 pagination links are displayed. If 22 search results are returned, 3 pagination 
+links are displayed.
 
 Pro Tip: To paginate the search results, try storing the search results in an array that can act as 
 a list, on which you can call your functions to show a page and append pagination links.
 
-Handle no results returned
+
+/*** NOT STARTED Handle no results returned
 If no matches are found by the search, include a message in the HTML to tell the user there are no matches.
 
 Note Don't use the built in alert() method here. The "No results" message must be printed to the page. ***/
 
 
 
-// Creates elements of the Search Bar
-const h2 = document.querySelector('h2');
-const studentSearchDiv = document.createElement('div');
-studentSearchDiv.classList.add("student-search");
-const studentSearchInput = document.createElement('input');
-studentSearchInput.placeholder = "Search for students...";
-const studentSearchButton = document.createElement('button');
-studentSearchButton.textContent = "Search";
-h2.insertAdjacentElement('afterend', studentSearchDiv);
-studentSearchDiv.appendChild(studentSearchInput);
-studentSearchDiv.appendChild(studentSearchButton);
 
 
-// Search Bar function
-const studentSearch = (searchInput, names) => {
-   for (let i = 0; i < names.length; i += 1) {
-      names[i].classList.remove("match");
-      if (0 !== studentSearchInput.value.toLowerCase() && studentSearchInput.value.toLowerCase() <= names[i].textContent.toLowerCase()) {
-         names[i].classList.add("match");
-      };
-   };
-};
 
-
-// Search Bar function invoked
-studentSearchInput.addEventListener('keyup', (event) => {
-   studentSearch(studentSearchInput, studentList);
-});
-studentSearchButton.addEventListener('click', (event) => {
-   event.preventDefault();
-   studentSearch(studentSearchInput, studentList);
-});
 
 
 
